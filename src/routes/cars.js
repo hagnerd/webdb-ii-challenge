6b00bc58148a272375492fdb189dc47944ee529a
Matt Hagner <web@matthagner.com>
2019-08-24 13:04:39 -0500

    Implement POST handler for api/cars

--- src/routes/cars.js
+++ src/routes/cars.js
@@ -29,6 +29,19 @@ async function validateId(req, res, next) {
   }
 }
 
+function validateInput(req, res, next) {
+  const { vin, make, model, mileage } = req.body;
+
+  if (!vin || !make || !model || !mileage) {
+    res.status(400).json({
+      message: "Please provide a vin, make, model and mileage"
+    });
+    return;
+  }
+
+  next();
+}
+
 router.get("/", async (_req, res) => {
   try {
     const accounts = await db.select("*").from("cars");
@@ -50,6 +63,21 @@ router.get("/:id", validateId, (req, res) => {
   res.json({
     car
   });
+});
+
+router.post("/", validateInput, async (req, res) => {
+  try {
+    const [id] = await db.from("cars").insert(req.body);
+
+    res.status(201).json({
+      id
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: "internal server error",
+      message: error.message
+    });
+  }
 });
 
 module.exports = router;

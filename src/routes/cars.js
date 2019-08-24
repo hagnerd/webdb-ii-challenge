ee7cace40823a5419d961a939f11266fe5e85dcc
Matt Hagner <web@matthagner.com>
2019-08-24 12:56:31 -0500

    Add middleware for validating car id

--- src/routes/cars.js
+++ src/routes/cars.js
@@ -3,6 +3,32 @@ const db = require("../../db");
 
 const router = express.Router();
 
+async function validateId(req, res, next) {
+  const { id } = req.params;
+
+  try {
+    const [car] = await db
+      .select("*")
+      .from("cars")
+      .where({ id });
+
+    if (car === undefined) {
+      res.status(404).json({
+        message: "invalid car id"
+      });
+      return;
+    }
+
+    req.car = car;
+    next();
+  } catch (error) {
+    res.status(500).json({
+      error: "internal server error",
+      message: error.message
+    });
+  }
+}
+
 router.get("/", async (_req, res) => {
   try {
     const accounts = await db.select("*").from("cars");
